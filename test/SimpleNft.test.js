236f2864f73e8d46c9964b978cfa07a703a63575
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-01-25 12:01:10 -0500

    start add test helper function and some tests

--- test/SimpleNft.test.js
+++ test/SimpleNft.test.js
@@ -1,62 +1,92 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-const { expect } = require("chai");
+const { time, loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { anyValue } = require('@nomicfoundation/hardhat-chai-matchers/withArgs');
+const { expect } = require('chai');
 
 const Helper = require('./shared');
+let contract;
 
-describe("Simple NFT", function () {
+describe('Simple NFT', function () {
   before(async function () {
-      [provider, owner, user1, user2, user3] = await Helper.setupProviderAndAccount();
+    [provider, owner, user1, user2, user3] = await Helper.setupProviderAndAccount();
   });
 
   beforeEach(async function () {
-      contract = await Helper.setupContract();
+    contract = await Helper.setupContract([user1.address, user2.address]);
   });
 
   it('Does contract owner can start minting? (should be)', async function () {
-    // Add test logic here
+    const currentTimestamp = await Helper.returnCurrentTimestamp();
+    await Helper.help_startMinting(contract, owner, currentTimestamp + 86400, currentTimestamp, true);
+    expect(await contract.isStarted()).to.be.equal(false);
+    expect(await contract.isWhiteListStarted()).to.be.equal(true);
   });
 
   it('Does anyone can start minting? (should not)', async function () {
-    // Add test logic here
+    const currentTimestamp = await Helper.returnCurrentTimestamp();
+    await Helper.help_startMinting(contract, user1, currentTimestamp + 86400, currentTimestamp, false);
+    expect(await contract.isStarted()).to.be.equal(false);
+    expect(await contract.isWhiteListStarted()).to.be.equal(false);
   });
 
-  it('Does contract owner can update contract uri? (should be)', async function () {
-    // Add test logic here
+  it('Does the user1 address of the WhiteList can mint a NFT? (it should)', async function () {
+    const currentTimestamp = await Helper.returnCurrentTimestamp();
+    await Helper.help_startMinting(contract, user1, currentTimestamp + 86400, currentTimestamp, false);
+    await Helper.help_isWhitelistValid(contract, user1, [...Helper.WhiteList, user1.address, user2.address], true);
   });
 
-  it('Does anyone can update contract uri? (should not)', async function () {
-    // Add test logic here
+  it('Does the user3 address of the WhiteList can mint a NFT? (should not)', async function () {
+    const currentTimestamp = await Helper.returnCurrentTimestamp();
+    await Helper.help_startMinting(contract, user3, currentTimestamp + 86400, currentTimestamp, false);
+    await Helper.help_isWhitelistValid(contract, user3, [...Helper.WhiteList, user1.address, user2.address], false);
   });
 
-  it('Does contract owner can update base uri? (should be)', async function () {
-    // Add test logic here
+  it('Does the user3 address of the WhiteList can mint a NFT even if he had himself to the proofs? (should not)', async function () {
+    const currentTimestamp = await Helper.returnCurrentTimestamp();
+    await Helper.help_startMinting(contract, user3, currentTimestamp + 86400, currentTimestamp, false);
+    await Helper.help_isWhitelistValid(contract, user3, [...Helper.WhiteList, user1.address, user2.address, user3.address], false);
   });
 
-  it('Does anyone can update base uri? (should not)', async function () {
-    // Add test logic here
-  });
+  // it('Does the contract name match the name use at deployment ?', async function () {
+  //   // Add test logic here
+  // });
 
-  it('Does contract owner can update contract uri and base uri? (should be)', async function () {
-    // Add test logic here
-  });
+  // it('Does the contract symbol match the symbol use at deployment ?', async function () {
+  //   // Add test logic here
+  // });
 
-  it('Does anyone can update contract uri and base uri? (should not)', async function () {
-    // Add test logic here
-  });
+  // it('Does contract owner can update contract uri? (should be)', async function () {
+  //   // Add test logic here
+  // });
 
-  it('Does contract owner can withdraw ether from contract? (should be)', async function () {
-    // Add test logic here
-  });
+  // it('Does anyone can update contract uri? (should not)', async function () {
+  //   // Add test logic here
+  // });
 
-  it('Does contract owner can withdraw ether from contract, after 1 first regular mint? (should be)', async function () {
-    // Add test logic here
-  });
+  // it('Does contract owner can update base uri? (should be)', async function () {
+  //   // Add test logic here
+  // });
 
-  it('Does anyone can withdraw ether from contract? (should not)', async function () {
-    // Add test logic here
-  });
+  // it('Does anyone can update base uri? (should not)', async function () {
+  //   // Add test logic here
+  // });
+
+  // it('Does contract owner can update contract uri and base uri? (should be)', async function () {
+  //   // Add test logic here
+  // });
+
+  // it('Does anyone can update contract uri and base uri? (should not)', async function () {
+  //   // Add test logic here
+  // });
+
+  // it('Does contract owner can withdraw ether from contract? (should be)', async function () {
+  //   // Add test logic here
+  // });
+
+  // it('Does contract owner can withdraw ether from contract, after 1 first regular mint? (should be)', async function () {
+  //   // Add test logic here
+  // });
+
+  // it('Does anyone can withdraw ether from contract? (should not)', async function () {
+  //   // Add test logic here
+  // });
 });
--- test/shared/functions.js
+++ test/shared/functions.js
@@ -6,78 +6,102 @@ const constants = require('../../constants');
 const methods = require('./methods');
 
 const buildRoot = async (addresses = constants.ADDRESSES) => {
-    const leaves = addresses.map((x) => keccak256(x));
-    const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
+  const leaves = addresses.map((x) => keccak256(x));
+  const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
 
-    const buf2hex = (x) => '0x' + x.toString('hex');
+  const buf2hex = (x) => '0x' + x.toString('hex');
 
-    const root = buf2hex(tree.getRoot());
+  const root = buf2hex(tree.getRoot());
 
-    return { tree, root };
+  return { tree, root };
 };
 
 const returnBuildRoot = async (addresses = constants.ADDRESSES) => {
-    const { root } = await buildRoot(addresses);
-    return root;
+  const { root } = await buildRoot(addresses);
+  return root;
 };
 
 const buildProof = async (address, addresses = constants.ADDRESSES) => {
-    const leaves = addresses.map((x) => keccak256(x));
-    const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
+  const leaves = addresses.map((x) => keccak256(x));
+  const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
 
-    const buf2hex = (x) => '0x' + x.toString('hex');
+  const buf2hex = (x) => '0x' + x.toString('hex');
 
-    const leaf = keccak256(address);
-    const proof = tree.getProof(leaf).map((data) => buf2hex(data.data));
+  const leaf = keccak256(address);
+  const proof = tree.getProof(leaf).map((data) => buf2hex(data.data));
 
-    return { tree, proof };
+  return { tree, proof };
 };
 
 const returnBuildProof = async (address, addresses = constants.ADDRESSES) => {
-    const { proof } = await buildProof(address, addresses);
-    return proof;
+  const { proof } = await buildProof(address, addresses);
+  return proof;
 };
 
 const sendRawTxn = async (input, sender, ethers, provider) => {
-    const txCount = await provider.getTransactionCount(sender.address);
-    const rawTx = {
-        chainId: network.config.chainId,
-        nonce: ethers.utils.hexlify(txCount),
-        to: input.to,
-        value: input.value || 0x00,
-        gasLimit: ethers.utils.hexlify(3000000),
-        gasPrice: ethers.utils.hexlify(25000000000),
-        data: input.data
-    };
-    const rawTransactionHex = await sender.signTransaction(rawTx);
-    const { hash } = await provider.sendTransaction(rawTransactionHex);
-    return await provider.waitForTransaction(hash);
+  const txCount = await provider.getTransactionCount(sender.address);
+  const rawTx = {
+    chainId: network.config.chainId,
+    nonce: ethers.utils.hexlify(txCount),
+    to: input.to,
+    value: input.value || 0x00,
+    gasLimit: ethers.utils.hexlify(3000000),
+    gasPrice: ethers.utils.hexlify(25000000000),
+    data: input.data
+  };
+  const rawTransactionHex = await sender.signTransaction(rawTx);
+  const { hash } = await provider.sendTransaction(rawTransactionHex);
+  return await provider.waitForTransaction(hash);
 };
 
 const checkRawTxnResult = async (input, sender, error) => {
-    let result;
-    if (error)
-        if (network.name === 'hardhat') await expect(sendRawTxn(input, sender, ethers, ethers.provider)).to.be.revertedWith(error);
-        else expect.fail('AssertionError: ' + error);
-    else {
-        result = await sendRawTxn(input, sender, ethers, ethers.provider);
-        expect(result.status).to.equal(1);
-    }
-    return result;
+  let result;
+  if (error)
+    if (network.name === 'hardhat') await expect(sendRawTxn(input, sender, ethers, ethers.provider)).to.be.revertedWith(error);
+    else expect.fail('AssertionError: ' + error);
+  else {
+    result = await sendRawTxn(input, sender, ethers, ethers.provider);
+    expect(result.status).to.equal(1);
+  }
+  return result;
 };
 
 const getRandomInt = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const returnCurrentTimestamp = async () => {
+  const currentBlockNumber = await provider.getBlockNumber();
+  const block = await provider.getBlock(currentBlockNumber);
+  return block.timestamp;
+};
+
+const help_startMinting = async (contract, sender, startPublicTimestamp, startWhiteListTimestamp, isValid) => {
+  const input = await contract.connect(sender).populateTransaction.startMinting(startPublicTimestamp, startWhiteListTimestamp);
+  if (isValid) {
+    await checkRawTxnResult(input, sender);
+  } else {
+    await checkRawTxnResult(input, sender, 'Ownable: caller is not the owner');
+  }
+};
+
+const help_isWhitelistValid = async (contract, sender, allAddresses, isValid) => {
+  const proofs = await returnBuildProof(sender.address, allAddresses);
+  const isWhitelistValid = await contract.connect(sender).isWhitelistValid(proofs);
+  expect(isWhitelistValid).to.be.equal(isValid);
 };
 
 module.exports = {
-    buildRoot,
-    returnBuildRoot,
-    buildProof,
-    returnBuildProof,
-    sendRawTxn,
-    checkRawTxnResult,
-    getRandomInt
+  buildRoot,
+  returnBuildRoot,
+  buildProof,
+  returnBuildProof,
+  sendRawTxn,
+  checkRawTxnResult,
+  getRandomInt,
+  returnCurrentTimestamp,
+  help_startMinting,
+  help_isWhitelistValid
 };
