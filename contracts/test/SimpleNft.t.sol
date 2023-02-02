e1b572d04d8e7f5646fc84d0a954d5ad6355b0c0
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-02-02 10:36:08 -0500

    add mint tests

--- contracts/test/SimpleNft.t.sol
+++ contracts/test/SimpleNft.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.9;
 
 import { Helper } from './shared/helper.t.sol';
+import { Errors } from './shared/errors.t.sol';
 
 contract SimpleNft_test is Helper {
   uint8 LOG_LEVEL = 0;
@@ -31,5 +32,29 @@ contract SimpleNft_test is Helper {
     vm.prank(ADMIN);
     nftContract.setBaseURI('test baseURI');
     assertEq(nftContract.baseURI(), 'test baseURI');
+  }
+
+  function test_SimpleNft_mint() public {
+    vm.prank(ADMIN);
+    vm.deal(ADMIN, 0.5 ether);
+    nftContract.mint{ value: 0.5 ether }(1);
+
+    assertEq(nftContract.ownerOf(0), ADMIN);
+  }
+
+  function test_SimpleNft_mint_2Tokens() public {
+    vm.prank(ADMIN);
+    vm.deal(ADMIN, 1 ether);
+    nftContract.mint{ value: 1 ether }(2);
+
+    assertEq(nftContract.ownerOf(0), ADMIN);
+    assertEq(nftContract.ownerOf(1), ADMIN);
+  }
+
+  function test_SimpleNft_mint_2Tokens_for_price_of_one() public {
+    vm.prank(ADMIN);
+    vm.deal(ADMIN, 0.5 ether);
+    verify_revertCall(Errors.RevertStatus.SimpleNftTransactionValueBelowMintPrice);
+    nftContract.mint{ value: 0.5 ether }(2);
   }
 }
